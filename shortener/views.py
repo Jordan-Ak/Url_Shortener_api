dca40cf46498819e8dd70ec90cc13670c4cfc64f
Jordan Akpoborie <JosiahDavid98@gmail.com>
2021-04-21 12:09:04 +0100

    Added Link viewset.

--- shortener/views.py
+++ shortener/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import reverse
+from rest_framework import viewsets
 
+from .models import Link
+from .serializers import LinkSerializer
 # Create your views here.
+
+
+class LinkViewSet(viewsets.ModelViewSet):
+    serializer_class = LinkSerializer
+
+    def get_queryset(self):
+
+        if self.request.user.is_staff:
+            queryset = Link.objects.all()
+        
+        else:
+            pass
+        
+        return queryset
