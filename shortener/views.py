28cd60a482e75d4da5f6b56893e94ef5eefbc9c1
Jordan Akpoborie <JosiahDavid98@gmail.com>
2021-04-21 15:03:36 +0100

    Made views redirect duplicate urls.

--- shortener/views.py
+++ shortener/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import reverse
+from django.http import HttpResponseRedirect
 from rest_framework import viewsets
+from rest_framework.response import Response
 
 from .models import Link
 from .serializers import LinkSerializer
@@ -20,9 +22,15 @@ class LinkViewSet(viewsets.ModelViewSet):
         
         return queryset
 
-    #def create(self, request, *args, **kwargs):
+    def create(self, request, *args, **kwargs):
  
-        #if "input_link" in request.POST:
-            #try:
-             #   instance = Link.objects.get(input_link = request.POST['input_link'])
-                #reverse()
+        if "input_link" in request.POST:
+            try:
+                instance = Link.objects.get(input_link = request.POST['input_link'])
+                return HttpResponseRedirect(reverse('LinkView-detail', args = [instance.UUID]))
+            
+            except Link.DoesNotExist:
+                serializer = LinkSerializer(data = request.data)
+                serializer.is_valid(raise_exception=True)
+                serializer.save()
+                return Response(serializer.data)
