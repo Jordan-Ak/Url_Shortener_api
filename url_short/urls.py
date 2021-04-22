6f7e08d57df0db1a9785b63a68ed1661f0f03826
Jordan Akpoborie <JosiahDavid98@gmail.com>
2021-04-22 22:40:55 +0100

    Added documentation.

--- url_short/urls.py
+++ url_short/urls.py
@@ -18,9 +18,28 @@ from django.urls import path, include
 from shortener.urls import router
 from shortener.views import Link_Redirector
 
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="URL-Shortener API",
+      default_version='v1',
+      description="Shortens your URLs",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@URLshortens.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('api/', include(router.urls)),
     
     path('api-auth', include('rest_framework.urls')),
